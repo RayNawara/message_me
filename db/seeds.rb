b88a44ae6eb6c10ff3b35c5e07d85ca3ab6cadda
Ray Nawara <rjnawara@gmail.com>
2019-03-17 13:03:15 -0400

    Render @messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Ray", password: "password")
+User.create(username: "Dan", password: "password")
+User.create(username: "Dave", password: "password")
+User.create(username: "Pete", password: "password")
+User.create(username: "Andrea", password: "password")
+Message.create(body: "Hi! I'm a new message", user: User.first)
+Message.create(body: "Hi! Me too!", user: User.find(2))
+Message.create(body: "Hi! Welcome!", user: User.find(3))
+Message.create(body: "Hi! Have fun!", user: User.find(4))
+Message.create(body: "Hi! Groovy!", user: User.find(5))
+Message.create(body: "Hi! Cool", user: User.find(1))
+Message.create(body: "Hi! Neat!", user: User.find(2))
